94b4a0f1efe22981ede0a52c6648b669d1292486
Aditi Tiwari <115400068+Aditiwari7@users.noreply.github.com>
2023-06-14 13:11:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const ele = document.getElementById("colorSelect");
+const btn = document.querySelector("form input");
+
+btn.addEventListener('click', ()=>{
+	let value = ele.value;
+	update(value);
+})
+
+function update(val){
+	if(!val){
+		return ;
+	}
+	for (let i = 0; i < ele.children.length; i++) {
+		if(val === ele.children[i].value){
+			ele.children[i].remove();
+			return;
+		}
+	}
+}
+
+// btn.addEventListener('click', ()=>{
+// 	let value = ele.value;
+// 	update(value);
+// })
